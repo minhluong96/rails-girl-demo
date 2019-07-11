3bbd77aece079f9127fb5a982e840f64c06b698c
minhluong96 <minhluong96@gmail.com>
2019-07-11 16:31:22 +0700

    Step 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'products#index'
   resources :products, only: [:index, :show]
+  namespace :admin do
+    root 'products#index'
+    resources :products, only: [:new, :create, :index, :edit, :update, :destroy]
+  end
 end
