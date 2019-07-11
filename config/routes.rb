cbba3361eecaec01feb788407fc3cf4409d9de6e
minhluong96 <minhluong96@gmail.com>
2019-07-11 14:28:03 +0700

    Reverse app

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'products/show'
-  root 'products#index'
-  resources :products, only: [:index, :show]
 
-  namespace :admin do
-    root 'products#index'
-    resources :products, only: [:new, :create, :index, :edit, :update, :destroy]
-  end
 end
