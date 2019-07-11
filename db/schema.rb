a574ca8a500c802be98cb73b668c2f2d006f4719
minhluong96 <minhluong96@gmail.com>
2019-07-11 15:31:56 +0700

    Step 4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_080322) do
+ActiveRecord::Schema.define(version: 2019_07_11_082329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_07_11_080322) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
 end
